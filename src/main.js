1e1778d5cd00e4cdef7762312b6fd3c034644da6
jiangli <1260067155@qq.com>
2022-11-14 16:51:38 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//加载全局样式表
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
-  <div id="app">
-<h1>hello world!</h1>
-  </div>
+  
 </template>
 
 <script>
-
 export default {
-  
 
 }
 </script>
